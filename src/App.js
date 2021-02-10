ba99e6e35e6f45c8bf1b74aeadc1adac07397a2a
beat-tech <usuario@PAVILION04-09>
2021-02-10 23:32:46 +0100

    Búsqueda y paginación funcionando #2

--- src/App.js
+++ src/App.js
@@ -1,59 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import placeholderImg from './placeholder.png'
-import { ReactComponent as ChevronLeft } from './chevron-left.svg'
-import { ReactComponent as ChevronRight } from './chevron-right.svg'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+import placeholderImg from './placeholder.png';
+import { ReactComponent as ChevronLeft } from './chevron-left.svg';
+import { ReactComponent as ChevronRight } from './chevron-right.svg';
 
 function App() {
-  const [searchResult, setSearchResult] = useState()
-  const [titleSearch, setTitleSearch] = useState ('king')
-  const [pageChange, setPageChange] = useState (1)
+  const [searchResult, setSearchResult] = useState();
+  const [titleSearch, setTitleSearch] = useState ('king');
+  const [pageChange, setPageChange] = useState (1);
 
-
- useEffect(() => {
+useEffect(() => {
     const search = async () => {
       const response = await fetch(
         `http://www.omdbapi.com/?apikey=a461e386&s=${titleSearch}&page=${pageChange}`,
       )
-
-      const data = await response.json()
-      setSearchResult (data)
-
-     
+      const data = await response.json();
+      setSearchResult (data);     
     }
-
-    search()
-    
-  },[titleSearch, pageChange])
+    search();
+  },[titleSearch, pageChange]);
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (e.target.title.value !==''){
-    setTitleSearch(e.target.title.value)}
-  
+    setTitleSearch(e.target.title.value)
+    }
   };
 
   const handleClickPrevious = () => {
     if (pageChange>1) {
-    setPageChange(pageChange-1)}
-  }
+    setPageChange(pageChange-1)
+    }
+  };
 
-  const handleClickNext = (e) => {
-    setPageChange (pageChange+1)
-  }
+  const handleClickNext = () => setPageChange (pageChange+1);
+
   return (
     <div className="App">
-      
-        <form  className="search" onSubmit= {handleSubmit}>
+      <form  className="search" onSubmit={handleSubmit}>
         <input name="title" type="text" placeholder="Search..." />
         <input className="button" value="Search" type="submit"></input>
-        </form>
-     
+      </form>
       {!searchResult ? (
         <p>No results yet</p>
       ) : (
         <div className="search-results">
-          <div className="chevron" onClick= {handleClickPrevious}>
+          <div className="chevron" onClick={handleClickPrevious}>
             <ChevronLeft />
           </div>
           <div className="search-results-list">
@@ -76,7 +69,7 @@ function App() {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
